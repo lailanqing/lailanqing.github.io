b76490e2487f90643d8974b6b638a261e00eafbe
lailanqing <1409202445@qq.com>
2025-06-15 18:02:08 +0800

    new_blog

---
+++ _posts/_site/2025-06-15-Mass-Energy.md
@@ -0,0 +1,5 @@
+$$
+\begin{aligned}
+\nabla \times \mathbf{E} = -\frac{\partial \mathbf{B}}{\partial t}
+\end{aligned}
+$$
--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Mass Energy",'url':"/2025/06/15/Mass-Energy.html"}]};
